b0fe8e145cfb97969001472d6d2c31f410824d5e
Astro <astro1247w@gmail.com>
2019-01-09 21:06:23 +0200

    Pre Entity Framework

---
+++ SQL_App1/DataModel/Base.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQL_App1.DataModel
+{
+    // Класс модели данных CodeFirst
+
+    public class Users
+    {
+        [Required] [Index] [Key]
+        public int Id { get; set; }
+        [Required] [MaxLength(255)] [ConcurrencyCheck]
+        public string Username { get; set; }
+        [Required] [MaxLength(255)] [ConcurrencyCheck]
+        public string Password { get; set; }
+    }
+
+    public class Chats
+    {
+        [Required] [Index] [Key]
+        public int Id { get; set; }
+        [Required]
+        public int Creator { get; set; }
+        [Required]
+        public int member { get; set; }
+        [Range(0, Double.MaxValue)]
+        public int MessagesCount { get; set; }
+    }
+
+    public class Keys
+    {
+        [Required] [Index]
+        public int Id { get; set; }
+        [Required]
+        public int OwnerId { get; set; }
+        [Required] [MaxLength(50)]
+        public string Key { get; set; }
+    }
+
+    public class Messages
+    {
+        [Required] [Index] [Key]
+        public int Id { get; set; }
+        [Required]
+        public int Sender { get; set; }
+        [Required]
+        public int ChatId { get; set; }
+
+        public string Message { get; set; }
+    }
+}
---
+++ SQL_App1/DataModel/BaseContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQL_App1.DataModel
+{
+    class BaseContext : DbContext
+    {
+        public BaseContext() : base("EfConnString")
+        {
+        }
+
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Chats> Chats { get; set; }
+        public DbSet<Keys> Keys { get; set; }
+        public DbSet<Messages> Messages { get; set; }
+    }
+}
